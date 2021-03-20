1275c6ba683e7759f283f861082d22fcbbc74850
s3b00 <s3boo@yandex.by>
2021-03-20 17:26:42 +0300

    first commit

---
+++ stack.js
@@ -0,0 +1,54 @@
+class Stack {
+    size = 0
+    elements = {}
+
+    constructor(max=10) {
+        max = parseInt(max) // проверка на целое число
+        if (isNaN(max) || !isFinite(max) || typeof max != 'number') throw new Error('Неподдерживаемый размер стека')
+        this.max = max
+    }
+
+    push(elem) {
+        if (this.size < this.max) {
+            this.size++
+            this.elements[this.size] = elem
+        } else throw new Error('Получается уже переполнение стека, дада!')
+    }
+
+    pop() {
+        if (this.size) {
+            let temp = this.elements[this.size]
+            
+            delete this.elements[this.size]
+            this.size--
+
+            return temp
+        } else throw new Error('А стек то пуст, они-чан!')
+    }
+
+    peek() {
+        if (this.size) {
+            return this.elements[this.size]
+        } else return null
+    }
+
+    isEmpty() {
+        return this.size == 0
+    }
+
+    toArray() { 
+        return Array.from(Object.values(this.elements))
+    }
+
+    fromIterable(iterable) {
+        if (!Array.isArray(iterable)) throw new Error('Неитерируемая сущность!')
+        
+        let resultStack = new Stack(this.max)
+
+        for (let item of iterable) { 
+            resultStack.push(item)
+        }
+
+        return resultStack
+    }
+}
